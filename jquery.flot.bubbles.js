5726967b3b93adcbf332742ff06737a640321886
Andreas Balster <a.balster@iss-automation.com>
2019-11-07 12:39:23 +0100

    Added radiusCallback function
    This can be used to modify the radius used for drawing the circle and the one used for hit testing with the same function

--- jquery.flot.bubbles.js
+++ jquery.flot.bubbles.js
@@ -250,7 +250,10 @@ THE SOFTWARE.
 				var y = points[iPoints - 1];
 				if (point[0] == x && point[1] == y) {
 					var radius_index = (iPoints - 2) / pointsize;
-					return parseInt(series.yaxis.scale * series.data[radius_index][2] / 2, 0);
+					let r = parseInt(series.yaxis.scale * series.data[radius_index][2] / 2, 0);
+                    if (series.bubbles.radiusCallback && (typeof (series.bubbles.radiusCallback) == "function"))
+                        r = series.bubbles.radiusCallback(r);
+                    return r;
 				}
 			}
 			return 0;
@@ -268,8 +271,10 @@ THE SOFTWARE.
 		function drawbubble(ctx, series, data, c, overlay) {
 			var x = offset.left + series.xaxis.p2c(data[0]);
 			var y = offset.top + series.yaxis.p2c(data[1]);
-			var v = data[2];
-			var r = parseInt(series.yaxis.scale * data[2] / 2, 0);
+            var v = data[2];
+            var r = parseInt(series.yaxis.scale * data[2] / 2, 0);
+            if (series.bubbles.radiusCallback && (typeof (series.bubbles.radiusCallback) == "function"))
+                r = series.bubbles.radiusCallback(r);
 			if (typeof c === 'function') {
 				c = c.apply(this, data);
 			}
